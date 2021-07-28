f093ff30c92a65dbdef7957b477f93e42fadaf95
igaki <github@musictale.net>
2021-07-29 00:56:38 +0900

    update program

--- win_single/ros/rps_game_ros.py
+++ win_single/ros/rps_game_ros.py
@@ -7,41 +7,43 @@ import rospy
 from std_msgs.msg import String
 from utils import RosWinMessenger
 
-def play_show_hand_game():
+def play_rps_game():
     rospy.sleep(3) 
     node_name = rospy.get_name()
-    # Prepare to play show hand game
-    # Specify topic names to commnicate with show hand game
+    # Prepare to play rps game
+    # Specify topic names to commnicate with rps game
     to_win_pub = rospy.Publisher("/from_ros", String, queue_size=1)
     messenger = RosWinMessenger(to_win_pub, "/from_windows")
     # Start game sequence
-    rospy.loginfo("%s:Try to start show hand game", node_name)
+    rospy.loginfo("%s:Try to start rps game", node_name)
 
     # Send game start signal to Windows, and wait Windows side response.
     message_from_win = messenger.wait_response(
-        "[rpsw]start", ["[rpsw]start"], timeout=30)
+        "[rps]start", ["[rps]start"], timeout=30)
     if message_from_win:
         rospy.loginfo("%s:Receive from win:%s",
                           node_name, message_from_win)
     else:
-        rospy.logerr("%s:Timeout. can't start show hand game on windows", node_name)
-        return "[rpsr]timeout"
+        rospy.logerr("%s:Timeout. can't start rps game on windows", node_name)
+        return "[rps]timeout"
 
     # Select robot's hand_type
-    hand_types = ["[rpsr]rock", "[rpsr]paper", "[rpsr]scissors"]
+    hand_types = ["[rps]rock", "[rps]paper", "[rps]scissors"]
     hand_type = random.choice(hand_types)
     rospy.loginfo("%s:Robot selects '%s'", node_name, hand_type)
 
-    # Send hand_type chosen by robot to windows show hand game, and wait game result
+    # Send robot's choice to windows game "[rps]rock", "[rps]paper", "[rps]scissors", and wait game result
+    # 1st argument: Message for publishing.
+    # 2nd argument: Target keywords to receive. Your program will wait until receiving one of the word in the list, ["[rps]win", "[rps]draw", "[rps]lose"].
     message_from_win = messenger.wait_response(
-        hand_type, ["[rpsw]win", "[rpsw]draw", "[rpsw]lose"], timeout=30)
+        hand_type, ["[rps]win", "[rps]draw", "[rps]lose"], timeout=30)
     if message_from_win:
         rospy.loginfo("%s:Receive from win:%s",
                     node_name, message_from_win)
     else:
         rospy.logerr(
-            "%s:Timeout. can't get show hand game result on windows", node_name)
-        return "[rpsr]timeout"
+            "%s:Timeout. can't get rps game result on windows", node_name)
+        return "[rps]timeout"
     rospy.sleep(3)
     return message_from_win                          
 
@@ -53,14 +55,13 @@ def demo():
     rospy.loginfo("%s:Started", rospy.get_name())
     rospy.sleep(10)
     node_name = rospy.get_name()
-    print("---rpsr---")
-    result = play_show_hand_game()
+    print("---rps---")
+    result = play_rps_game()
     rospy.sleep(10)
 
-    #node.process()
     rospy.loginfo("%s:Exiting", rospy.get_name())
 
-# If you want to test this program only, please execute "rosrun oit_pbl_ros_samples show_hand_game_ros.py" 
+# If you want to test this program only, please execute "rosrun oit_pbl_ros_samples rps_game_ros.py" 
 if __name__ == '__main__':
     try:
         demo()
--- win_single/win/rps_game_win.py
+++ win_single/win/rps_game_win.py
@@ -1,69 +1,82 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #!!!!!This program must be executed on windows python3 environment!!!!!
-
+from rosbridge_tcp import RosBridgeTCP
 import random
 
-def show_hand(win_msg):
-    hand_types = ["rock", "paper", "scissors"]
-    hand_type = random.choice(hand_types)
-
-    return hand_type
-
+## Let's modify the code to correctly judge rock-paper-scissors.
 def judge_game(win_hand, ros_hand):
     print("win_hand:", win_hand, "\tros_hand:", ros_hand)
 
     result = "[rps]win"
-    #result = "[rps]lose"
-    #result = "[rps]draw"
 
     return result
 
-def start_game(topic_name_from_win, ros_bridge_tcp):
-    # definition of message types receiving from ros
-    hand_types = ["[rps]rock", "[rps]paper", "[rps]scissors"]
-
-    # Send start message and wait hand type selected by ROS
-    pub_msg = {
-        "op": "publish",
+def main():
+	# Wrapper class for TCP/IP communication
+    ros_bridge_tcp = RosBridgeTCP()
+    # Prepare to publish ROS topic from Windows
+    topic_name_from_win = "/from_windows"
+    advertise_msg = {
+        "op": "advertise",
         "topic": topic_name_from_win,
-        "msg": {"data": "[rps]OK_start"}
+        "type": "std_msgs/String"
     }
-    message_from_ros = ros_bridge_tcp.wait_response(pub_msg, hand_types, timeout=30)
-    if message_from_ros:
-        print("\nReceive from ROS:", message_from_ros, "\n")
-
-    # Decide your hand
-    window_message = "Rock, Paper, Scissors, Go!"
-    win_hand = show_hand(window_message)
-
-    # Judge
-    result = judge_game(win_hand, message_from_ros)
-    pub_msg = {
-        "op": "publish",
-        "topic": topic_name_from_win,
-        "msg": {"data": result}
+    ros_bridge_tcp.send_message(advertise_msg) # Send advertise message to ROS
+    # Prepare to subscribe to ROS topic
+    topic_name_from_ros = "/from_ros"
+    subscribe_msg = {
+        "op": "subscribe",
+        "topic": topic_name_from_ros,
+        "type": "std_msgs/String"
     }
-    # Send game result to ROS.
-    print(result)
-    # In this program, ros never return to the following wait_response
-    ros_bridge_tcp.wait_response(pub_msg, ["[rps]OK_result"], timeout=5)
+    ros_bridge_tcp.send_message(subscribe_msg)
 
+    print("Wait ROS messages...")
 
-def demo():
-    # Decide ROS hand
-    hand_types = ["[rps]rock", "[rps]paper", "[rps]scissors"]
-    message_from_ros = random.choice(hand_types)
-    print("\nReceive from ROS:", message_from_ros, "\n")
+    while True:
+        messages = ros_bridge_tcp.wait() #Wait for message from ROS and assign the response into 'messages'
+        for message in messages:
+            # If message['msg']['data'] is "[rps]start", rps_game starts.
+            if message['msg']['data'] == "[rps]start":
+                # definition of message types receiving from ros
+                hand_types = ["[rps]rock", "[rps]paper", "[rps]scissors"]
 
-    # Decide your hand
-    window_message = "Rock, Paper, Scissors, Go!"
-    win_hand = show_hand(window_message)
+                # Send start message and wait hand type selected by ROS
+                pub_msg = {
+                    "op": "publish",
+                    "topic": topic_name_from_win,
+                    "msg": {"data": "[rps]start"}
+                }
+                # 1st argument: Message for publishing.
+                # 2nd argument: Target keywords to receive. Your program will wait until receiving one of the word in the list, 'hand_types'.
+                message_from_ros = ros_bridge_tcp.wait_response(pub_msg, hand_types, timeout=30)
+                if message_from_ros:
+                    print("\nReceive from ROS:", message_from_ros, "\n")
 
-    # Judge
-    result = judge_game(win_hand, message_from_ros)
-    print("\nWindows:", result, "\n")
+                # Decide your hand
+                win_hand = random.choice(hand_types)
+                # Judge
+                result = judge_game(win_hand, message_from_ros)
+                pub_msg = {
+                    "op": "publish",
+                    "topic": topic_name_from_win,
+                    "msg": {"data": result}
+                }
+                # Send game result to ROS.
+                print(result)
+                # In this program, ros never return to the following wait_response
+                ros_bridge_tcp.wait_response(pub_msg, ["[rps]result"], timeout=5)
+                break
+        else:
+            continue
+        break
 
+    try:
+        ros_bridge_tcp.terminate()
+        ros_bridge_tcp = None
+    except Exception as e:
+        print(str(e))
 
-if __name__=="__main__":
-    demo()
+if __name__ == '__main__':
+    main()

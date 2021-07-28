b7584dfec31fae3ce018f75d9b66c09b5e1cb967
ykama615 <83806782+ykama615@users.noreply.github.com>
2021-07-28 13:18:28 +0900

    Add files via upload

---
+++ win_single/ros/start_on_ros_single_rps.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import math
+import os
+import random
+import rospy
+from std_msgs.msg import String
+from utils import RosWinMessenger
+import rps_game_ros as rpsr
+
+def end_game():
+    rospy.sleep(3)
+    node_name = rospy.get_name()
+    # Specify topic names to commnicate with windows side
+    to_win_pub = rospy.Publisher("/from_ros", String, queue_size=1)
+    messenger = RosWinMessenger(to_win_pub, "/from_windows")
+    rospy.loginfo("%s:Try to end game", node_name)
+
+    # Send The end signal to Windows, and wait Windows side response.
+    message_from_win = messenger.wait_response(
+        "The end", None, timeout=30)
+    if message_from_win:
+        rospy.loginfo("%s:Receive from win:%s",
+                      node_name, message_from_win)
+    else:
+        rospy.logerr("%s:Timeout. can't end Windows game process.", node_name)
+
+def process():
+    rospy.sleep(10)
+    node_name = rospy.get_name()
+
+    print("---rpsr---")
+    result = rpsr.play_show_hand_game()
+    rospy.sleep(5)
+    end_game()
+    rospy.loginfo("/* GAME:%s */", result)
+
+def main():
+    script_name = os.path.basename(__file__)
+    rospy.init_node(os.path.splitext(script_name)[0])
+    rospy.sleep(0.5)  # rospy.Time.now() returns 0, without this sleep.
+
+    rospy.loginfo("%s:Started", rospy.get_name())
+
+    process()
+    rospy.loginfo("%s:Exiting", rospy.get_name())
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as e:
+        rospy.logerr("%s:%s", rospy.get_name(), str(e))
+        exit(1)
